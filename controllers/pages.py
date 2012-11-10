84362b66095819024a7f6e5b16b4f5240ae0d2d0
Andrey Kuzmin <unsoundscapes@gmail.com>
2012-11-11 02:15:20 +0400

    Moved depency to models/page

--- controllers/pages.py
+++ controllers/pages.py
@@ -5,7 +5,7 @@ import os
 import datetime
 import json
 from config import config
-from base import db, auth, flash, load_navigation
+from base import db, auth, flash
 from modules.translation import _, N_
 from modules.utils import dthandler
 from template import render, render_partial, link_to
