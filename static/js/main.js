15a8cb31ba372b696954a35acee642812026353a
Andrey Kuzmin <unsoundscapes@gmail.com>
2012-11-19 21:15:17 +0400

    Consistent quotes

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,11 @@
 require.config({
-  urlArgs: "bust=" + (new Date).getTime(),
+  urlArgs: 'bust=' + (new Date).getTime(),
   paths: {
-    stonegarden: "/a/config",
-    jquery: "../vendor/js/jquery-1.8.1",
-    "jquery-tinymce": "../vendor/js/tiny_mce/jquery.tinymce",
-    backbone: "../vendor/js/backbone-0.9.2",
-    underscore: "../vendor/js/underscore-1.4.2"
+    stonegarden: '/a/config',
+    jquery: '../vendor/js/jquery-1.8.1',
+    'jquery-tinymce': '../vendor/js/tiny_mce/jquery.tinymce',
+    backbone: '../vendor/js/backbone-0.9.2',
+    underscore: '../vendor/js/underscore-1.4.2'
   },
   shim: {
     'jquery-tinymce': ['jquery'],
