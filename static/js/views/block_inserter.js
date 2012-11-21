d1180a714522d9ebdb84c6adf3f16cb49ef86f5b
Andrey Kuzmin <unsoundscapes@gmail.com>
2012-11-21 13:12:05 +0400

    Api to create blocks

--- static/js/views/block_inserter.js
+++ static/js/views/block_inserter.js
@@ -2,10 +2,11 @@ define(["jquery"
       , "underscore"
       , "backbone"
       , "stonegarden"
-      , "views/context_menu"], function ($, _, Backbone, sg) {
+      , "views/contextmenu"], function ($, _, Backbone, sg) {
 
   var views = sg.views || (sg.views = {});
 
+  // TODO: generate context menu using view
 
   views.BlockInserter = Backbone.View.extend({
 
@@ -14,7 +15,8 @@ define(["jquery"
     template: _.template($("#block-inserter-template").html()),
 
     events: {
-      "mousemove": "mousemoveEvent"
+      "mousemove": "mousemoveEvent",
+      "click .js-create": "createBlock"
     },
 
     mousemoveEvent: function(e) {
@@ -24,6 +26,21 @@ define(["jquery"
     render: function() {
       this.$el.html(this.template());
       return this;
+    },
+
+    createBlock: function(e) {
+      var $link = $(e.currentTarget)
+        , template = $link.data("template")
+        , type = $link.data("type");
+
+      e.preventDefault();
+
+      this.trigger("block:create", {
+        template: template,
+        type: type,
+        position: this.$el.parent().children().index(this.el) + 1
+      });
+
     }
 
   });
--- static/js/views/block_placeholder.js
+++ static/js/views/block_placeholder.js
@@ -1,21 +1,33 @@
 define(["jquery"
       , "underscore"
       , "backbone"
-      , "stonegarden"
-      , "views/context_menu"], function ($, _, Backbone, sg) {
+      , "stonegarden"], function ($, _, Backbone, sg) {
 
   var views = sg.views || (sg.views = {});
 
 
   views.BlockPlaceholder = Backbone.View.extend({
 
-    className: "sg-placeholder js-placeholder",
+    className: "sg-block-placeholder js-placeholder",
 
     template: _.template($("#block-placeholder-template").html()),
+
+    events: {
+      "click": "createBlock"
+    },
 
     render: function() {
       this.$el.html(this.template());
       return this;
+    },
+
+    createBlock: function(e) {
+      e.preventDefault();
+      this.trigger("block:create", {
+        template: "content",
+        type: "wysiwyg",
+        position: 1
+      });
     }
 
   });
