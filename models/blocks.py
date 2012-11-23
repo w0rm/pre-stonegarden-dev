7bafb18a7a6a396baa375a754e5be6c33c652d46
Andrey Kuzmin <unsoundscapes@gmail.com>
2012-11-23 13:39:14 +0400

    Removed unnecessary code (we will use localstorage as buffer)

--- models/blocks.py
+++ models/blocks.py
@@ -3,7 +3,6 @@ import json
 from base import db
 from modules.utils import dthandler
 from template import render_block, smarty, sanitize
-from models.pages import load_page_data, get_page_by_id
 
 
 def update_block_by_id(block_id, data):
@@ -110,22 +109,6 @@ def delete_block_by_id(block_id):
     )
 
     return block
-
-
-def save_block_in_session(block):
-    """Removes ids attributes and saves block in session"""
-    remove_attributes = "id", "block_id", "blocks", "page_id", "level"
-    copied_block = web.storage((k, v) for k, v in block.iteritems()
-                               if k not in remove_attributes)
-    web.ctx.session.buffer = web.storage(block=copied_block)
-    return copied_block
-
-
-def get_block_from_session():
-    """Retreives block from session"""
-    # We have to copy dict, because it can be changed later
-    # in code and we don't want the changes in session
-    return web.storage(web.ctx.session.buffer.block)
 
 
 def get_block_by_id(block_id):
