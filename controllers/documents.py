9381166778646e53d831f591541d14fa7f739f3d
Andrey Kuzmin <unsoundscapes@gmail.com>
2012-11-26 04:08:25 +0400

    Introduce api to manage documents

--- controllers/documents.py
+++ controllers/documents.py
@@ -1,33 +1,10 @@
 # coding: utf-8
-
 import web
-import os
-import json
-import urllib
+from base import auth
+from template import link_to, image_url
 from modules.form import ApiForm, notnull
-from config import config
-from base import db, auth, flash
-from modules.translation import _, N_
 from modules.restful_controller import RESTfulController
-from template import render, render_partial, link_to, image_url
-from modules.form import ApiForm, notnull
-from web import ctx
-from pytils.translit import slugify
-import random
-import string
-import mimetypes
-
-document_download_forbidden = N_("Cannot download this document")
-
-
-def document_download_access(func):  # defines who can download file
-    def proxyfunc(iself, *args, **kw):
-        document = args[0]
-        if auth.has_role("admin", "editor", "user") or document.is_published:
-            return func(iself, *args, **kw)
-        else:
-            raise flash.redirect(document_download_forbidden)
-    return proxyfunc
+from models.documents import *
 
 
 class Storage:
@@ -39,15 +16,7 @@ class Storage:
 class Download:
 
     def GET(self, filename):
-        document = db.select(
-            "documents",
-            locals(),
-            where="filename = $filename AND NOT is_deleted",
-            limit=1)[0]
-        return self.download(document)
-
-    @document_download_access
-    def download(self, document):
+        document = get_document_by_filename()
         return download_document(document)
 
 
@@ -57,6 +26,7 @@ class Documents(RESTfulController):
         web.form.Input("parent_id", notnull),
         web.form.Input("title"),
         web.form.Input("type"),
+        web.form.Input("position"),
         web.form.Input("description"),
         web.form.Input("is_published"),
         web.form.Input("upload"),
@@ -67,7 +37,7 @@ class Documents(RESTfulController):
         web.form.Input("type"),
     )
 
-    @auth.restrict("admin", "editor")
+    @auth.restrict("admin", "editor", "user")
     def list(self):
         """Lists documents by specified parent_id (or type filter)"""
         form = self.filter_form()
@@ -88,13 +58,18 @@ class Documents(RESTfulController):
             raise web.seeother(link_to("documents", document))
         raise form.validation_error()
 
+    @auth.restrict("admin", "editor", "user")
+    def get(self, document_id):
+        document = get_document_by_id(document_id)
+        web.header("Content-Type", "application/json")
+        return document_to_json(document)
+
     @auth.restrict("admin", "editor")
     def update(self, document_id):
-        d = web.input(page_id=None, sizes=[])
         form = self.form()
-        if form.validates(d):
+        if form.validates():
             document = update_document_by_id(document_id, form.d)
-            raise web.seeother(link_to("document_ids", document))
+            raise web.seeother(link_to("documents", document))
         raise form.validation_error()
 
     @auth.restrict("admin", "editor")
@@ -104,154 +79,7 @@ class Documents(RESTfulController):
         return '{"status": 1}'
 
 
-#TODO: extract parts from the code below to models/documents.py
-
-class DropUploadDocument:
-
-    @auth.restrict("admin", "editor")
-    def POST(self):
-        get = web.input(document_id=1, obj_id=None, obj_type=None,
-                        image_size="t", filename=None, upload=None)
-        folder = db.select("documents", get, where="id = $document_id",
-                           limit=1)[0]
-        web.header("Content-Type", "application/json")
-        i = web.webapi.rawinput()
-        try:
-            files = i.files
-            if not isinstance(files, list):
-                files = [files]
-            for f in files:
-                if f.filename:
-                    mimetype, encoding = mimetypes.guess_type(f.filename)
-                    with db.transaction():
-                        filename, filesize = save_document(f.file)
-
-                        position = db.select(
-                            "documents",
-                            folder,
-                            where='parent_id = $id AND NOT '
-                                  'type = "folder" AND NOT is_deleted',
-                            what='MAX(position)+1 as n',
-                        )[0].n
-
-                        file_id = db.insert(
-                            "documents",
-                            parent_id=folder.id,
-                            position=position or 1,
-                            filename=filename,
-                            ids=(folder.ids + "," + str(folder.id)
-                                 if folder.ids else folder.id),
-                            level=folder.level + 1,
-                            created_at=datetime.datetime.now(),
-                            title=get.filename or os.path.splitext(
-                            f.filename)[0],
-                            extension=os.path.splitext(f.filename)[1].lower(),
-                            mimetype=mimetype,
-                            type=("image" if "image" in
-                                  mimetype else "document"),
-                            filesize=filesize,
-                            is_published=True,
-                        )
-
-                    if get.upload == "image":
-                        if "image" in mimetype:
-                            raise web.seeother(
-                                web.url(
-                                    "/a/documents/%d/image_size" % file_id,
-                                    size=get.image_size))
-                        raise web.HTTPError('400 Bad Request', dict(),
-                                            json.dumps(dict(status=0)))
-
-                    if get.obj_type in ("contacts", "buildings"):
-                        if "image" in mimetype:
-                            db.update(get.obj_type, where="id=$obj_id",
-                                      vars=get, photo_id=file_id)
-                            raise web.seeother(
-                                web.url(
-                                    "/a/documents/%d/image_size" % file_id,
-                                    size=get.image_size))
-                        raise web.HTTPError('400 Bad Request', dict(),
-                                            json.dumps(dict(status=0)))
-            raise web.seeother(link_to("documents", folder) + ".json")
-        except KeyError:
-            pass
-        raise web.HTTPError('400 Bad Request', dict(),
-                            json.dumps(dict(status=0)))
-
-
-class NewFolderDocument:
-
-    @auth.restrict("admin", "editor")
-    def POST(self):
-        get = web.input(document_id=1, title=None)
-        folder = db.select("documents", get, where="id = $document_id",
-                           limit=1)[0]
-        web.header("Content-Type", "application/json")
-        with db.transaction():
-            document = web.storage(
-                parent_id=get.document_id,
-                ids=(folder.ids + "," + str(folder.id)
-                                  if folder.ids else folder.id),
-                level=folder.level + 1,
-                created_at=datetime.datetime.now(),
-                title=get.title,
-                type="folder",
-                is_published=True,
-            )
-            document.id = db.insert("documents", **document)
-        raise web.seeother(link_to("documents", document) + ".json")
-
-
-class GetDocument:
-
-    @auth.restrict("admin", "editor", "user")
-    def GET(self, document_id, format=None):
-        document = db.select(
-            "documents",
-            locals(),
-            where="id = $document_id AND NOT is_deleted",
-            limit=1)[0]
-        if format == ".json":
-            web.header("Content-Type", "application/json")
-            document.html = unicode(render_partial.ui.document(document))
-            return json.dumps(document, default=dthandler)
-
-        document.path = db.select(
-            "documents",
-            where="id in (%s) AND NOT is_deleted" % document.ids,
-        ).list() if document.level > 0 else []
-
-        if document.type == "folder":
-            document.folders = db.select(
-                "documents",
-                locals(),
-                where="parent_id = $document_id AND "
-                      "type = 'folder' AND NOT is_deleted",
-                order="title",
-            ).list()
-            if "filter" in web.input():
-                document.documents = db.select(
-                    "documents",
-                    locals(),
-                    where="parent_id = $document_id AND NOT type IN"
-                          " ('folder','document') AND NOT is_deleted",
-                    order="position").list()
-            else:
-                document.documents = db.select(
-                    "documents",
-                    locals(),
-                    where="parent_id = $document_id AND NOT "
-                          "type = 'folder' AND NOT is_deleted",
-                    order="position").list()
-            if web.ctx.env.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
-                return render_partial.ui.documents(document)
-            else:
-                return render.documents.list(document)
-        else:
-            # download document
-            return download_document(document)
-
-
+#TODO: Better api for image resize
 class GetImageSize:
 
     @auth.restrict("admin", "editor", "user")
@@ -272,105 +100,3 @@ class GetImageSize:
                                    title=document.title))
         else:
             raise web.seeother(src)
-
-
-class OrderDocument:
-
-    @auth.restrict("admin", "editor")
-    def POST(self, document_id):
-
-        document = db.select(
-            "documents",
-            locals(),
-            where="id = $document_id AND NOT is_deleted",
-            limit=1)[0]
-        document.position_new = int(web.input(position=None).position)
-
-        with db.transaction():
-            if document.position_new < int(document.position):
-                db.update(
-                    "documents",
-                    where="parent_id = $document_id AND position >= "
-                          "$position_new AND position < $position AND "
-                          "NOT is_deleted AND NOT type = 'folder' ",
-                    vars=document,
-                    position=web.db.SQLLiteral("position+1"),
-                )
-            if document.position_new > int(document.position):
-                db.update(
-                    "documents",
-                    where="parent_id = $document_id AND position > "
-                          "$position AND position <= $position_new AND "
-                          "NOT is_deleted AND NOT type = 'folder'",
-                    vars=document,
-                    position=web.db.SQLLiteral("position-1"),
-                )
-            db.update("documents", where="id = $id", vars=document,
-                      position=document.position_new)
-
-        if web.ctx.env.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
-            web.header("Content-Type", "application/json")
-            return json.dumps(dict(status=1))
-        else:
-            raise web.seeother(web.ctx.env.get('HTTP_REFERER', '/'))
-
-
-class EditDocumentSettings:
-
-    @auth.restrict("admin", "editor")
-    def POST(self, document_id):
-        # page_id should be set
-        document = db.select(
-            "documents",
-            locals(),
-            where="id = $document_id AND NOT is_deleted",
-            limit=1)[0]
-        if document.is_system:
-            # cannot edit or delete system files and folders
-            raise flash.error(
-                _("Cannot edit or delete system files and folders."))
-        document_form = documentSettingsForm(web.input())
-        if document_form.valid:
-            db.update(
-                "documents",
-                where="id = $document_id",
-                vars=locals(),
-                updated_at=datetime.datetime.now(),
-                **document_form.d)
-            raise web.seeother(link_to("documents", document) + ".json")
-        return "NOT OK"
-
-
-class DeleteDocument:
-
-    @auth.restrict("admin", "editor")
-    def POST(self, document_id):
-
-        document = db.select(
-            "documents",
-            locals(),
-            where="id = $document_id AND NOT is_deleted",
-            limit=1)[0]
-
-        if document.is_system:
-            # cannot edit or delete system files and folders
-            raise flash.error(
-                _("Cannot edit or delete system files and folders."))
-
-        # shift positions
-        db.update(
-            "documents",
-            where="parent_id = $parent_id AND position > $position AND "
-                  "NOT is_deleted AND NOT type = 'folder'",
-            vars=document,
-            position=web.SQLLiteral("position-1"),
-        )
-
-        # delete recursively
-        delete_document(document)
-
-        if web.ctx.env.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
-            web.header("Content-Type", "application/json")
-            return json.dumps(dict(status=1))
-        else:
-            raise web.seeother(web.ctx.env.get('HTTP_REFERER', '/'))
--- models/documents.py
+++ models/documents.py
@@ -1,33 +1,197 @@
 import os
 import web
 import json
+import mimetypes
+import random
+import string
 from base import db
 from modules.utils import dthandler
+from modules.translation import _
 from config import config
 
 
+def create_document(document):
+    """Creates new document and saves upload"""
+
+    parent = get_document_by_id(data.parent_id)
+
+    document.update(
+        ids=parent.ids + "," + str(parent.id),
+        level=parent.level + 1,
+        position=int(document.position),
+        parent_id=int(parent_id),
+        created_at=web.SQLLiteral("CURRENT_TIMESTAMP"),
+        user_id=auth.get_user().id,
+        is_published=True,
+    )
+
+    if document.type == "folder":
+        del document["upload"]
+    else:
+        upload = document.pop("upload")
+        try:
+            mimetype, encoding = mimetypes.guess_type(upload.filename)
+            filename, filesize = save_document(upload.file)
+            document.update(
+                title=document.title or os.path.splitext(upload.filename)[0],
+                filename=filename,
+                extension=os.path.splitext(f.filename)[1].lower(),
+                mimetype=mimetype,
+                type="image" if "image" in mimetype else "document",
+                filesize=filesize,
+            )
+        except:
+            raise flash.error(_("File upload error."))
+
+    # Shift positions to free the space to insert document
+    db.update(
+        "documents",
+        where="parent_id = $parent_id AND position >= $position AND "
+              "NOT is_deleted",
+        vars=document,
+        position=web.SQLLiteral("position + 1"),
+    )
+
+    document.id = db.insert("documents", **document)
+
+    # TODO: fix created_at
+    return document
+
+
 def update_document_by_id(document_id, data):
-    pass
 
+    document = get_document_by_id(document_id)
 
-def create_document(data):
-    pass
+    parent = get_document_by_id(data.parent_id)
+
+    data.update(
+        ids=parent.ids + "," + str(parent.id),
+        level=parent.level + 1,
+        position=int(data.position),
+        parent_id=int(parent_id),
+        updated_at=web.SQLLiteral("CURRENT_TIMESTAMP"),
+    )
+
+    # Cannot delete system files and folders
+    if document.is_system:
+        raise flash.error(
+            _("Cannot edit or delete system files and folders."))
+
+    # TODO: wrap the code below in transaction
+    # TODO: custom input field that returns integer value
+    if data.position != document.position or data.parent_id != document.parent_id:
+        # Collapse positions
+        db.update(
+            "documents",
+            where="parent_id = $parent_id AND position > $position AND "
+                  "NOT is_deleted",
+            vars=document,
+            position=web.SQLLiteral("position - 1"),
+        )
+        # Shift positions to free the space to insert document
+        db.update(
+            "documents",
+            where="parent_id = $parent_id AND position >= $position AND "
+                  "NOT is_deleted",
+            vars=data,
+            position=web.SQLLiteral("position + 1"),
+        )
+        # TODO: recursively update ids and level for inner blocks
+
+    # Cannot change documents type and file
+    del data["type"]
+    del data["upload"]
+
+    db.update(
+        "documents",
+        where="id = $document_id",
+        vars=locals(),
+        **data)
+
+    # Update document with data
+    # TODO: fix updated_at
+    document.update(data)
+
+    return document
 
 
 def delete_document_by_id(document_id):
-    pass
+    document = get_document_by_id(document_id)
+
+    # cannot delete system files and folders
+    if document.is_system:
+        raise flash.error(
+            _("Cannot edit or delete system files and folders."))
+
+    # Collapse positions
+    db.update(
+        "documents",
+        where="parent_id = $parent_id AND position > $position AND "
+              "NOT is_deleted",
+        vars=document,
+        position=web.SQLLiteral("position - 1"),
+    )
+
+    # delete recursively
+    return delete_document_tree(document)
+
+
+def delete_document_tree(document):
+    """
+        Recursively deletes document tree branch.
+        Ignores is_system flag, deletes everything.
+    """
+    db.update("documents", where="id = $document_id AND NOT is_deleted",
+              vars=document, is_deleted=1)
+    if document.type == "folder":
+        for doc in db.select("documents", what="id, type",
+                             where="parent_id = $document_id AND NOT is_deleted",
+                             vars=locals()):
+            delete_document_tree(doc)
+    return document
+
+
+def get_document_path(document):
+    return db.select(
+        "documents",
+        vars=document,
+        where="id in $ids AND NOT is_deleted",
+    ).list() if document.ids else []
+
+
+def get_document_by_id(document_id):
+    return db.select(
+        "documents",
+        locals(),
+        where="id = $document_id AND NOT is_deleted",
+        limit=1,
+    )[0]
+
+
+def get_document_by_filename(filename):
+    return db.select(
+        "documents",
+        locals(),
+        where="filename = $filename AND NOT is_deleted",
+        limit=1,
+    )[0]
 
 
 def get_documents_by_parent_id(parent_id, type=None):
-    """Select and return documents"""
-    return []
+    """Select and return documents from parent folder"""
+    #TODO: filter by type
+    return db.select("documents", locals(),
+                     where="parent_id = $parent_id AND NOT is_deleted",
+                     order="position ASC").list()
+
+
+def document_to_json(document):
+    return json.dumps(document, default=dthandler, sort_keys=True, indent=2)
 
 
 def documents_to_json(documents):
     return json.dumps(documents, default=dthandler, sort_keys=True, indent=2)
 
-
-#TODO: review the code below:
 
 def rem_file(filename):
     if os.path.exists(filename):
@@ -40,15 +204,6 @@ def delete_file(filename):
     for size in config.image.keys():
         rem_file(os.path.join(config.static_dir, filename +
                               "_" + size + ".jpg"))
-
-
-def delete_document(document):
-    db.update("documents", where="id = $id AND NOT is_deleted",
-              vars=document, is_deleted=1)
-    if document.type == "folder":
-        for doc in db.select("documents", document,
-                             where="parent_id=$id AND NOT is_deleted"):
-            delete_document(doc)
 
 
 def save_document(f):
@@ -81,10 +236,13 @@ def save_document(f):
 
 
 def download_document(document):
+
+    if not auth.has_role("admin", "editor", "user") and not document.is_published:
+        raise flash.redirect(_("Cannot download this document"))
+
     web.header("Content-Disposition", "attachment; filename=%s" %
                slugify(document.title) + document.extension)
     web.header("Content-Type", document.mimetype)
-    #web.header('Transfer-Encoding', 'chunked')
     f = open(os.path.join(config.upload_dir, document.filename), 'rb')
     while 1:
         buf = f.read(1024 * 8)
