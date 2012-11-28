d5c44e7028509096520dae3226ae70d6a7262ff5
Andrey Kuzmin <unsoundscapes@gmail.com>
2012-11-28 17:47:28 +0400

    Queue file uploads

--- static/js/models/documents/documents.js
+++ static/js/models/documents/documents.js
@@ -17,6 +17,7 @@ define(["jquery"
 
     initialize: function() {
       this.on("add remove", this.updatePositions, this);
+      this.ajaxQueue = $({});
     },
 
     updatePositions: function(model, collection, options) {
@@ -26,13 +27,71 @@ define(["jquery"
     },
 
     getUploadPosition: function() {
-      var firstNonFolder = this.find(function(d) {
-            return d.get("type") != "folder";
-          })
-        , position = firstNonFolder ?
-            firstNonFolder.get("position") :
-            1;
+      // Returns position (index+1) after the last folder
+      var position = 1;
+
+      this.each(function(d, index) {
+        if (d.get("type") === "folder") {
+          position = index + 2
+        };
+      });
+
       return position;
+    },
+
+    create: function(model, options) {
+      var jqXHR
+        , dfd = $.Deferred()
+        , promise = dfd.promise()
+        , coll = this;
+
+      // queue our ajax request
+      this.ajaxQueue.queue(doRequest);
+
+      // add the abort method
+      promise.abort = function(statusText) {
+        // Proxy abort to the jqXHR if it is active
+        if (jqXHR) {
+          return jqXHR.abort(statusText);
+        }
+
+        // If there wasn't already a jqXHR we need to remove from queue
+        var queue = this.ajaxQueue.queue()
+          , index = $.inArray(doRequest, queue);
+
+        if (index > -1) {
+          queue.splice(index, 1);
+        }
+
+        // Reject the deferred
+        dfd.rejectWith(options.context || options,
+                       [promise, statusText, ""]);
+
+        return promise;
+      };
+
+      // Run the actual collection.create
+      function doRequest(next) {
+        options = options ? _.clone(options) : {};
+        model = coll._prepareModel(model, options);
+        if (!model) return false;
+        if (!options.wait) coll.add(model, options);
+        var success = options.success;
+        options.success = function(nextModel, resp, xhr) {
+          if (options.wait) coll.add(nextModel, options);
+          if (success) {
+            success(nextModel, resp);
+          } else {
+            nextModel.trigger('sync', model, resp, options);
+          }
+        };
+        jqXHR = model.save(null, options)
+          .done(dfd.resolve)
+          .fail(dfd.reject)
+          .then(next, next);
+      };
+
+      return promise;
     }
 
   });
