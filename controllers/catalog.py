dc55a1ee7ebce98ea224ae4b73904f6a56ac838e
Andrey Kuzmin <unsoundscapes@gmail.com>
2013-02-12 14:04:49 +0400

    Dont include static file twice

--- controllers/catalog.py
+++ controllers/catalog.py
@@ -16,16 +16,14 @@ from models.pages import *
 
 EXPORT_PAGE_TYPES = ["catalog", "category", "product"]
 
-STATIC_INCLUDES = """
-fonts
+STATIC_INCLUDES = """fonts
 img
 vendor/js/foundation
 vendor/js/jquery-1.8.1.js
 vendor/js/require-2.1.1.js
 css/style.css
 js/plugins
-js/public_main.js
-""".split()
+js/public_main.js""".split()
 
 PATCH_ATTRIBUTES = ["src", "href", "data-main", "data-m_src", "data-l_src"]
 INCLUDE_STATIC = ["src", "data-m_src", "data-l_src"]
@@ -41,7 +39,7 @@ def add_string(tar, string, name):
 
 def trim_querystring(path):
     q = path.rfind("?")
-    if q:
+    if q >= 0:
         return path[0:q]
     else:
         return path
@@ -86,9 +84,12 @@ class Export:
             "pages",
             what="path",
             where="`type` IN $types AND NOT is_deleted " +
+                  #"AND path = $path " +
                   page_where(with_auth_check=False),
-            #limit=4,
-            vars={"types": EXPORT_PAGE_TYPES}
+            vars={
+                "types": EXPORT_PAGE_TYPES,
+                #"path": "/catalog/microelectronics/diodyi/2d136a91"
+            }
         )
 
         try:
@@ -103,29 +104,35 @@ class Export:
             html = urllib2.urlopen(web.ctx.home + p.path).read()
             soup = BeautifulSoup(html)
             soup, files = format_soup(soup, p.path)
-            all_files += files
+            # Add html page to archive
             add_string(tar, str(soup), p.path + ".html")
+            # Add static files to archive
+            all_files += files
 
-        all_files += STATIC_INCLUDES
+        # Add static files to set
+        for name in STATIC_INCLUDES:
+            sys_path = config.rootdir + "/static/" + name
+            if os.path.isdir(sys_path):
+                for root, dirnames, filenames in os.walk(sys_path):
+                    for filename in filenames:
+                        all_files.append(
+                            substract_root(
+                                root + "/" + filename,
+                                config.rootdir + "/static/"
+                            )
+                        )
+            else:
+                all_files.append(name)
+
         all_files = set(all_files)
+        # Add static files to archive
         for name in all_files:
             try:
                 tar_path = "static/" + name
                 sys_path = config.rootdir + "/" + tar_path
-                if os.path.isdir(sys_path):
-                    for root, dirnames, filenames in os.walk(sys_path):
-                        for filename in filenames:
-                            tar.add(
-                                root + "/" + filename,
-                                substract_root(
-                                    root + "/" + filename,
-                                    config.rootdir + "/"
-                                )
-                            )
-                else:
-                    tar.add(sys_path, tar_path)
+                tar.add(sys_path, tar_path)
             except:
-                pass
+                raise
 
         tar.close()
         return (u"<p><a href=\"/static/static.tar\">Теперь скачайте "
